ee5ad8bb04553e5a0f06c34bd76bd3b1acb7402d
kuminaruse <eternelbeaute2010@gmail.com>
2020-06-18 14:19:04 +0900

    modify-chatfront

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,31 @@
+$('.js-form').on('submit', function(){
+    console.log('hoge');
+  });
+
+$('.js-form').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+});
+
+def create
+@message = @group.messages.new(message_params)
+if @message.save
+  respond_to do |format|
+    format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+    format.json
+  end
+else
+  @messages = @group.messages.includes(:user)
+  flash.now[:alert] = 'メッセージを入力してください。'
+  render :index
+end
+end
