24579743d1d9489ea56ffeccb35955d1de988c7f
kuminaruse <eternelbeaute2010@gmail.com>
2020-06-18 15:06:36 +0900

    modify1

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -17,15 +17,13 @@ $('.js-form').on('submit', function(e){
 });
 
 def create
-@message = @group.messages.new(message_params)
-if @message.save
-  respond_to do |format|
-    format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
-    format.json
+  @message = @group.messages.new(message_params)
+  if @message.save
+    respond_to do |format|
+      format.html { redirect_to group_messages_path(@group), notice: 'メッセージが送信されました' }
+      format.json
+    end
+  else
+    @messages = @group.messages.includes(:user)
   end
-else
-  @messages = @group.messages.includes(:user)
-  flash.now[:alert] = 'メッセージを入力してください。'
-  render :index
-end
 end
