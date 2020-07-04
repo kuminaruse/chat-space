793b2b633f58cc5efbe2b881ded5c01cd9fb75ba
kuminaruse <eternelbeaute2010@gmail.com>
2020-07-04 16:36:54 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,14 @@
 lock "~> 3.14.1"
 
 set :application, "chat-space"
-set :repo_url, "git@github.com:<kuminaruse>/<chat-space>.git"
+set :repo_url, "git@github.com:kuminaruse/chat-space.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' #カリキュラム通りに進めた場合、2.5.1か2.3.1です
 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/key_pem.pem'] 
+                  keys: ['~/.ssh/ChatSpace.pem'] 
 
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 
